33d0bce7b557849106b8bd9ca32f8755952a1dde
weslleyFA <weslleyfaustino2013@gmail.com>
2022-07-22 16:42:55 -0300

    Visao do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import xadrez.PartidaXadrez;
+
 public class Program {
     public static void main(String[] args) {
-
+        PartidaXadrez novaPartida = new PartidaXadrez();
+        UI.printTabuleiro(novaPartida.getPecas());
     }
 }
